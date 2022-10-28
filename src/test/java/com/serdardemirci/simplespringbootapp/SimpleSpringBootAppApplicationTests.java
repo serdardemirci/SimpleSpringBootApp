5a3f3c9ff31940d44973cbc88d80fc297bcd32aa
Serdar Demirci <oezcan.serdar.demirci@exxeta.com>
2022-10-28 14:09:26 +0200

    Initial commit

---
+++ src/test/java/com/serdardemirci/simplespringbootapp/SimpleSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serdardemirci.simplespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
